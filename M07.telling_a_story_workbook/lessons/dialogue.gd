b700ff3ee6f465b979909f53b2ad854f4461b308
eddiespaget <edsalas@s.sfusd.edu>
2025-10-31 13:37:11 -0700

    M7L4 almost complete

---
+++ M07.telling_a_story_workbook/lessons/dialogue.gd
@@ -0,0 +1,10 @@
+extends Control
+@onready var rich_text_label: RichTextLabel = %RichTextLabel
+@onready var next_button: Button = %NextButton
+var dialogue_items := [
+	"I'm hungry",
+	"I'll stay hungry",
+	"Forever hungry",
+]
+
+#Typed Arrays
